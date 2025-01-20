05c14d129b4484226bb52acbd94d512cb135c150
Ankit Verma <ankitv78389@gmail.com>
2025-01-20 23:59:05 +0530

    :hammer:[refractor][added challenge 7]

---
+++ challenge 7 - simple food restaurant/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () =>{
+    return(
+            <header>
+                    <div className = "button-container">
+                        <button>Our Menu</button> 
+                        <button>About</button> 
+                        <button>Hours & Location</button>
+                    </div> 
+                </header>
+    )
+}
+
+export default Header
