8b53c9193e3d3ab0111ac843d1a6c4fe2d551656
Ankit Verma <ankitv78389@gmail.com>
2025-02-20 00:04:15 +0530

    :hammer:[refractor][challenge 37 solution minor changes]

--- challenge 37 - debatable/components/DebatePost.jsx
+++ challenge 37 - debatable/components/DebatePost.jsx
@@ -1,11 +1,10 @@
-import React from "react"
-import PostComments from "./PostComments"
-import PostContent from "./PostContent"
-import postData from "../postData"
-import {nanoid} from "nanoid"
+import React from "react";
+import PostComments from "./PostComments";
+import PostContent from "./PostContent";
+import postData from "../postData";
+import { nanoid } from "nanoid";
 export default function DebatePost() {
-     
-/* Challenge 
+  /* Challenge 
 
 The form doesn't work. Your task is to make it a controlled form that adds a comment to the post when the user clicks "Send."
 
@@ -24,66 +23,68 @@ The form doesn't work. Your task is to make it a controlled form that adds a com
     5. Your code can live entirely inside this file, although you are welcome to move things around 
        if you'd like. 
 */
-    
-    const [comments, setComments] = React.useState(postData.comments)
-    const[formData,setFormData]= React.useState({
-        id:nanoid(),
-        username:"",
-        commentText:"",
-        isAnonymous:false
-        
-    })
-    const handleChange = (e)=>{
-        // console.log(e.target.value)
-        const {name,value,checked,type}=e.target
-        setFormData((prevData)=> ({...prevData,[name]:type==="checked"?checked:value}))
-    }
-    
-    const handleSubmit=(e)=>{
-        event.preventDefault()
-        postData.comments.push(formData)
-        setComments([...postData.comments])
-        setFormData({
-          id: nanoid(),
-          userName: "",
-          isAnonymous: false,
-          commentText:""
-        })
-    }
-    return (
-        <div className="post-container">
-            <PostContent data={{...postData}} />
-            <PostComments data={comments} />
-            <form onSubmit={handleSubmit}>
-                <input
-                    className="text-input"
-                    type="text"
-                    placeholder="Enter username."
-                    name="username"
-                    value={formData.username}
-                    onChange={handleChange}
-                    required
-                />
-                <textarea
-                    placeholder="What do you think?"
-                    name="commentText"
-                    value={formData.commentText}
-                    onChange={handleChange}
-                    required
-                />
-                <label>
-                    <input 
-                        className="checkbox"
-                        type="checkbox"
-                        checked={formData.isAnonymous}
-                        onChange={handleChange}
-                        name="isAnonymous"
-                        
-                    />
-                    Post anonymously?
-                </label>
-                <button>Send</button>
-            </form>
-        </div>
-    )
+
+  const [comments, setComments] = React.useState(postData.comments);
+  const [formData, setFormData] = React.useState({
+    id: nanoid(),
+    userName: "",
+    commentText: "",
+    isAnonymous: false,
+  });
+  const handleChange = (e) => {
+    // console.log(e.target.value)
+    const { name, value, checked, type } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === "checked" ? checked : value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    postData.comments.push(formData);
+    setComments([...postData.comments]);
+    setFormData({
+      id: nanoid(),
+      userName: "",
+      isAnonymous: false,
+      commentText: "",
+    });
+  };
+
+  return (
+    <div className="post-container">
+      <PostContent data={{ ...postData }} />
+      <PostComments data={comments} />
+      <form onSubmit={handleSubmit}>
+        <input
+          className="text-input"
+          type="text"
+          placeholder="Enter username."
+          name="userName"
+          value={formData.userName}
+          onChange={handleChange}
+          required
+        />
+        <textarea
+          placeholder="What do you think?"
+          name="commentText"
+          value={formData.commentText}
+          onChange={handleChange}
+          required
+        />
+        <label>
+          <input
+            className="checkbox"
+            type="checkbox"
+            checked={formData.isAnonymous}
+            onChange={handleChange}
+            name="isAnonymous"
+          />
+          Post anonymously?
+        </label>
+        <button>Send</button>
+      </form>
+    </div>
+  );
 }
